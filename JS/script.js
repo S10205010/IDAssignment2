c2488281d5db5608732b4f9ed1441c796491d1c8
S10205010 <73088550+S10205010@users.noreply.github.com>
2020-12-25 20:31:30 +0800

    Edit Light Dark Mode

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,38 @@
-var modeCondition = false;
+var modeCondition;
 $("#LightDarkMode").click(function () {
-  // false means not dark mode  
-  if (modeCondition == false) {
+  // false means not dark mode
+  if (modeCondition == 'false'){
+    localStorage.setItem("LightDarkMode", true);
+    modeCondition = "true";
+  }else{
+    localStorage.setItem("LightDarkMode", false);
+    modeCondition = "false";
+  }
+  lightDark(modeCondition);
+});
+$(document).ready(function () {
+  if (localStorage.getItem("LightDarkMode") == null) {
+    modeCondition = 'true';
+  } else {
+    modeCondition = localStorage.getItem("LightDarkMode");
+  }
+  lightDark(modeCondition);
+  
+});
+function lightDark(modeCondition){
+  if (modeCondition == "false") {
     $("header nav").removeClass("bg-light navbar-light");
     $("header nav").addClass("bg-dark navbar-dark");
     $("body").css({ color: "gray", "background-color": "black" });
     $("#LightDarkMode").css({ "background-color": "black" });
     $(".navbar-brand img").attr("src", "Images/CarpeDiemDark.png");
-    modeCondition= true;
+    
   } else {
     $("header nav").removeClass("bg-dark navbar-dark");
     $("header nav").addClass("bg-light navbar-light");
     $("body").css({ color: "black", "background-color": "white" });
     $("#LightDarkMode").css({ "background-color": "white" });
     $(".navbar-brand img").attr("src", "Images/CarpeDiemLight.png");
-    modeCondition = false;
+    
   }
-});
+}
