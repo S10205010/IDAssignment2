14ca77bf190e7f7706ed0354e48cb379f5cdd563
S10205010 <73088550+S10205010@users.noreply.github.com>
2020-12-27 19:05:28 +0800

    Edited index.js

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,16 @@
-
 $(document).ready(function () {
   var weaFc2Hr = JSON.parse(sessionStorage.getItem("2hourly"));
   // This portion is to add values into dropdown option.
-  for(let i = 0; i < weaFc2Hr.area_metadata.length;i++){
-    let temp = weaFc2Hr.area_metadata[i].name
-    let disp = "<option value = '"+i+"'>"+temp+"</option>"
+  for (let i = 0; i < weaFc2Hr.area_metadata.length; i++) {
+    let temp = weaFc2Hr.area_metadata[i].name;
+    let disp = "<option value = '" + i + "'>" + temp + "</option>";
     $("#location").append(disp);
   }
   var index;
-  $("select").click(function(){
-    index = $("#location").val()
-    console.log(index)
-  })
-
+  $("select").click(function () {
+    index = $("#location").val();
+    console.log(index);
+  });
 });
 // Function to process location and output display
 // Pythagoras function to calculate distance.
@@ -23,23 +21,21 @@ function closestDist(lat, long, lat1, long1) {
   return result;
 }
 //Function to find closest station/reading point for weather
-function closestPoint(ip,weaFc2Hr){
+function closestPoint(ip, weaFc2Hr) {
   //user Latidude and Longitude
   let lat = ip.latitude;
   let long = ip.longitude;
   let smallestDist = 10;
   let indexWeaFc2Hr;
-  for (let i = 0;i<weaFc2Hr.area_metadata.length;i++)
-  {
+  for (let i = 0; i < weaFc2Hr.area_metadata.length; i++) {
     //station's latitude and longitude
-    let lat1=weaFc2Hr.area_metadata[i].label_location.latitude;
-    let long1= weaFc2Hr.area_metadata[i].label_location.longitude;
-    let distDiff = closestDist(lat,long,lat1,long1);
-    if (distDiff <= smallestDist){
+    let lat1 = weaFc2Hr.area_metadata[i].label_location.latitude;
+    let long1 = weaFc2Hr.area_metadata[i].label_location.longitude;
+    let distDiff = closestDist(lat, long, lat1, long1);
+    if (distDiff <= smallestDist) {
       smallestDist = distDiff;
-      indexWeaFc2Hr=i; 
-    }
-    else{
+      indexWeaFc2Hr = i;
+    } else {
       continue;
     }
   }
@@ -65,12 +61,11 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
   data: { date_time: date_time, date: date_d },
 }).done(function (data) {
-  if (sessionStorage.getItem("2hourly") != null) {
-    sessionStorage.clear();
-  } else {
-    console.log("null");
-  }
-  sessionStorage.setItem("2hourly", JSON.stringify(data));
+  if ((data.api_info.status == "healthy")) {
+    sessionStorage.setItem("2hourly", JSON.stringify(data));
+  }else{
+    alert("Failed to load API! ")
+  }  
 });
 $.ajax({
   type: "GET",
@@ -79,11 +74,9 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/rainfall",
   data: { date_time: date_time, date: date_d },
 }).done(function (data) {
-  if (sessionStorage.getItem("2hourly") != null) {
-    sessionStorage.clear();
-  } else {
-    console.log("null");
-  }
-  sessionStorage.setItem("2hourly", JSON.stringify(data));
+  if ((data.api_info.status == "healthy")) {
+    sessionStorage.setItem("rainfall", JSON.stringify(data));
+  }else{
+    alert("Failed to load API! ")
+  }   
 });
-
