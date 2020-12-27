f01e649d436f89a7544ee42aa3c4f3246c630780
S10205010 <73088550+S10205010@users.noreply.github.com>
2020-12-27 17:23:11 +0800

    Removed ipapi due to mixed content

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,20 @@
 
 $(document).ready(function () {
   var weaFc2Hr = JSON.parse(sessionStorage.getItem("2hourly"));
-  var ip = JSON.parse(sessionStorage.getItem("IP"));
-  console.log(ip,weaFc2Hr)
-  var closestWeaFc2Hr = closestPoint(ip,weaFc2Hr);
-  $("#location").text(weaFc2Hr.area_metadata[closestWeaFc2Hr].name)
-});
+  // This portion is to add values into dropdown option.
+  for(let i = 0; i < weaFc2Hr.area_metadata.length;i++){
+    let temp = weaFc2Hr.area_metadata[i].name
+    let disp = "<option value = '"+i+"'>"+temp+"</option>"
+    $("#location").append(disp);
+  }
+  var index;
+  $("select").click(function(){
+    index = $("#location").val()
+    console.log(index)
+  })
 
+});
+// Function to process location and output display
 // Pythagoras function to calculate distance.
 function closestDist(lat, long, lat1, long1) {
   let xDiff = lat - lat1;
@@ -64,61 +72,18 @@ $.ajax({
   }
   sessionStorage.setItem("2hourly", JSON.stringify(data));
 });
-
-//IP portion
-// var test = {
-//   "ip": "112.199.235.83",
-//   "type": "ipv4",
-//   "continent_code": "AS",
-//   "continent_name": "Asia",
-//   "country_code": "SG",
-//   "country_name": "Singapore",
-//   "region_code": "01",
-//   "region_name": null,
-//   "city": "Singapore",
-//   "zip": null,
-//   "latitude": 1.3200000524520874,
-//   "longitude": 103.8198013305664,
-//   "location": {
-//     "geoname_id": 1880252,
-//     "capital": "Singapore",
-//     "languages": [
-//       {
-//         "code": "en",
-//         "name": "English",
-//         "native": "English"
-//       },
-//       {
-//         "code": "ms",
-//         "name": "Malay",
-//         "native": "Bahasa Melayu"
-//       },
-//       {
-//         "code": "ta",
-//         "name": "Tamil",
-//         "native": "தமிழ்"
-//       },
-//       {
-//         "code": "zh",
-//         "name": "Chinese",
-//         "native": "中文"
-//       }
-//     ],
-//     "country_flag": "http://assets.ipapi.com/flags/sg.svg",
-//     "country_flag_emoji": "🇸🇬",
-//     "country_flag_emoji_unicode": "U+1F1F8 U+1F1EC",
-//     "calling_code": "65",
-//     "is_eu": false
-//   }
-// }
-fetch("https://api.ipapi.com/check?access_key=fcc0cefb939e2977b8aa4f6143767571&format=1")
-.then(response=>response.json())
-.then(function(data){
-  if (sessionStorage.getItem("IP")== null){
-  sessionStorage.setItem("IP",JSON.stringify(data))}
-  else{
-    sessionStorage.setItem("IP",null)
+$.ajax({
+  type: "GET",
+  dataType: "json",
+  contentType: "text/plain",
+  url: "https://api.data.gov.sg/v1/environment/rainfall",
+  data: { date_time: date_time, date: date_d },
+}).done(function (data) {
+  if (sessionStorage.getItem("2hourly") != null) {
+    sessionStorage.clear();
+  } else {
+    console.log("null");
   }
-})
-// if (sessionStorage.getItem("IP")== null){
-// sessionStorage.setItem("IP",JSON.stringify(test))}
+  sessionStorage.setItem("2hourly", JSON.stringify(data));
+});
+
