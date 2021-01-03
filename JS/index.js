5d453682db64d76ca314a1ad38163b98d1998f2a
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-01-04 00:05:50 +0800

    Added JS for 24 weather forecast

--- JS/index.js
+++ JS/index.js
@@ -82,19 +82,6 @@ const failed24hrFC = {
       periods: [
         {
           time: {
-            start: "0000-00-00T18:00:00+08:00",
-            end: "0000-00-00T06:00:00+08:00",
-          },
-          regions: {
-            west: "Thundery Showers",
-            east: "Thundery Showers",
-            central: "Thundery Showers",
-            south: "Thundery Showers",
-            north: "Thundery Showers",
-          },
-        },
-        {
-          time: {
             start: "0000-00-00T06:00:00+08:00",
             end: "0000-00-00T12:00:00+08:00",
           },
@@ -117,6 +104,19 @@ const failed24hrFC = {
             central: "Partly Cloudy (Day)",
             south: "Thundery Showers",
             north: "Partly Cloudy (Day)",
+          },
+        },
+        {
+          time: {
+            start: "0000-00-00T18:00:00+08:00",
+            end: "0000-00-00T06:00:00+08:00",
+          },
+          regions: {
+            west: "Thundery Showers",
+            east: "Thundery Showers",
+            central: "Thundery Showers",
+            south: "Thundery Showers",
+            north: "Thundery Showers",
           },
         },
       ],
@@ -242,12 +242,10 @@ if (date.getMonth() < 9) {
   month = date.getMonth() + 1;
 }
 if (date.getDate() < 9) {
-  day = "0" + (date.getMonth() + 1);
+  day = "0" + date.getDate();
 } else {
-  day = date.getDate() + 1;
+  day = date.getDate();
 }
-console.log(month);
-console.log(day);
 var date_time =
   date.getFullYear() +
   "-" +
@@ -256,8 +254,8 @@ var date_time =
   day +
   "T" +
   date.toLocaleTimeString();
-var date_d = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-console.log(date_time);
+var date_d = date.getFullYear() + "-" + month + "-" + day;
+
 //API request section
 //API Rainfall
 //Once retrieve API data, it is stored into Session storage.
@@ -268,14 +266,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/rainfall",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`Rainfall API ${request} ${status.status}`);
+    console.log(`Rainfall API ${request} ${status.status}`);
     sessionStorage.setItem("rainfall", JSON.stringify(failedAPI));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items[0].timestamp != "") {
     sessionStorage.setItem("rainfall", JSON.stringify(data));
   } else {
-    alert("Failed to load Rainfall API!");
+    console.log("Failed to load Rainfall API!(API data error)");
     sessionStorage.setItem("rainfall", JSON.stringify(failedAPI));
   }
 });
@@ -287,14 +285,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/air-temperature",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`Air Temperature API ${request} ${status.status}`);
+    console.log(`Air Temperature API ${request} ${status.status}`);
     sessionStorage.setItem("airTemp", JSON.stringify(failedAPI));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items[0].timestamp != "") {
     sessionStorage.setItem("airTemp", JSON.stringify(data));
   } else {
-    alert("Failed to load Air Temperature API!");
+    console.log("Failed to load Air Temperature API!(API data error)");
     sessionStorage.setItem("airTemp", JSON.stringify(failedAPI));
   }
 });
@@ -306,14 +304,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/relative-humidity",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`Relative Humidity API ${request} ${status.status}`);
+    console.log(`Relative Humidity API ${request} ${status.status}`);
     sessionStorage.setItem("relativeHumidity", JSON.stringify(failedAPI));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items[0].timestamp != "") {
     sessionStorage.setItem("relativeHumidity", JSON.stringify(data));
   } else {
-    alert("Failed to load Relative Humidity API!");
+    console.log("Failed to load Relative Humidity API! (API data error)");
     sessionStorage.setItem("relativeHumidity", JSON.stringify(failedAPI));
   }
 });
@@ -325,14 +323,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/wind-direction",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`Wind Direction API ${request} ${status.status}`);
+    console.log(`Wind Direction API ${request} ${status.status}`);
     sessionStorage.setItem("WindDirection", JSON.stringify(failedAPI));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items[0].timestamp != "") {
     sessionStorage.setItem("WindDirection", JSON.stringify(data));
   } else {
-    alert("Failed to load Wind Direction API!");
+    console.log("Failed to load Wind Direction API! (API data error)");
     sessionStorage.setItem("WindDirection", JSON.stringify(failedAPI));
   }
 });
@@ -344,14 +342,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/wind-speed",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`Wind Speed API ${request} ${status.status}`);
+    console.log(`Wind Speed API ${request} ${status.status}`);
     sessionStorage.setItem("WindSpeed", JSON.stringify(failedAPI));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items[0].timestamp != "") {
     sessionStorage.setItem("WindSpeed", JSON.stringify(data));
   } else {
-    alert("Failed to load Wind Speed API!");
+    console.log("Failed to load Wind Speed API! (API data error)");
     sessionStorage.setItem("WindSpeed", JSON.stringify(failedAPI));
   }
 });
@@ -363,14 +361,14 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`2 Hour Weather Forecast API ${request} ${status.status}`);
+    console.log(`2 Hour Weather Forecast API ${request} ${status.status}`);
     sessionStorage.setItem("02HourFC", JSON.stringify(failed2hrFC));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.area_metadata.length != 0) {
     sessionStorage.setItem("02HourFC", JSON.stringify(data));
   } else {
-    alert("Failed to load 2 Hour Weather Forecast API!");
+    console.log("Failed to load 2 Hour Weather Forecast API! (API data error)");
     sessionStorage.setItem("02HourFC", JSON.stringify(failed2hrFC));
   }
 });
@@ -382,15 +380,17 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`24 Hour Weather Forecast API ${request} ${status.status}`);
-    sessionStorage.setItem("rainfall", JSON.stringify(failed24hrFC));
+    console.log(`24 Hour Weather Forecast API ${request} ${status.status}`);
+    sessionStorage.setItem("24HourFC", JSON.stringify(failed24hrFC));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items.length != 1) {
     sessionStorage.setItem("24HourFC", JSON.stringify(data));
   } else {
-    alert("Failed to load 24 Hour Weather Forecast API!");
-    sessionStorage.setItem("rainfall", JSON.stringify(failed24hrFC));
+    console.log(
+      "Failed to load 24 Hour Weather Forecast API! (API data error)"
+    );
+    sessionStorage.setItem("24HourFC", JSON.stringify(failed24hrFC));
   }
 });
 //API 4 days weather forecast
@@ -401,18 +401,20 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/4-day-weather-forecast",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`4 Day Weather Forecast API ${request} ${status.status}`);
+    console.log(`4 Day Weather Forecast API ${request} ${status.status}`);
     sessionStorage.setItem("4DayFC", JSON.stringify(failed4dayFC));
   },
 }).done(function (data) {
-  if (data.api_info.status == "healthy") {
+  if (data.items.length != 1) {
     sessionStorage.setItem("4DayFC", JSON.stringify(data));
   } else {
-    alert("Failed to load 4 Day Weather Forecast API!");
+    console.log("Failed to load 4 Day Weather Forecast API! (API data error)");
     sessionStorage.setItem("4DayFC", JSON.stringify(failed4dayFC));
   }
 });
 //API for PSI
+//Plans to get full day readings
+//chart as a function
 $.ajax({
   type: "GET",
   dataType: "json",
@@ -420,17 +422,9 @@ $.ajax({
   url: "https://api.data.gov.sg/v1/environment/psi",
   data: { date_time: date_time, date: date_d },
   error: function (status, request) {
-    alert(`PSI API ${request} ${status.status}`);
-    sessionStorage.setItem("psi", JSON.stringify(failedPSI));
+    console.log(`PSI API ${request} ${status.status}`);
   },
-}).done(function (data) {
-  if (data.api_info.status == "healthy") {
-    sessionStorage.setItem("psi", JSON.stringify(data));
-  } else {
-    alert("Failed to load 4 Day Weather Forecast API!");
-    sessionStorage.setItem("psi", JSON.stringify(failedPSI));
-  }
-});
+}).done(function (data) {});
 //ajaxStop is used so that all ajax is requests is completed before
 //execution
 $(document).ajaxStop(function () {
@@ -448,39 +442,105 @@ $(document).ajaxStop(function () {
   var weaFc4d = JSON.parse(sessionStorage.getItem("4DayFC"));
   //Variable to be used by main program
   var area = weaFc2Hr.area_metadata;
-  $("#locationSearch").keyup(function(){
+  //Loading of 24Hour information
+  load24Hour(weaFc24Hr);
+  //Search function
+  $("#locationSearch").keyup(function () {
     //Get input from user
     $("#location div").remove();
     let userValue = $("#locationSearch").val().toUpperCase();
     let userLen = userValue.length;
     let indexResult = [];
     //Search index of related areas
-    for(i = 0; i < area.length; i ++){
-      let areaname = area[i].name.slice(0,userLen).toUpperCase();
-      if(userValue == areaname){
-        indexResult.push(i);        
-      }
-      else{
+    for (i = 0; i < area.length; i++) {
+      let areaname = area[i].name.slice(0, userLen).toUpperCase();
+      if (userValue == areaname) {
+        indexResult.push(i);
+      } else {
         continue;
       }
     }
     //From index establish latitude and longitude of each respective location
-    for(i = 0;i<indexResult.length;i++){
+    for (i = 0; i < indexResult.length; i++) {
       let index = indexResult[i];
       let lat = area[index].label_location.latitude;
       let long = area[index].label_location.longitude;
       //Labeling location
       let name = area[index].name;
       console.log(name);
-      $("#location").append("<div></div>")
+      $("#location").append("<div></div>");
       currentWeather(name);
     }
-  })
+  });
 });
 
-function currentWeather(name){
-  let disp = "<h3>"+name+"</h3>"
-  $("#location div").append(disp)
+function load24Hour(weaFc24Hr) {
+  let general = weaFc24Hr.items[0].general
+  //Weather(Air Temp)
+  $("#t24High").text(general.temperature.high +" °C")
+  $("#t24Low").text(general.temperature.low+" °C")
+  // Weather (Relative Humidity)
+  $("#rh24High").text(general.relative_humidity.low+" %")
+  $("#rh24Low").text(general.relative_humidity.low+" %")
+  //Weather (Wind Speed)
+  $("#ws24High").text(general.wind.speed.low+" knots")
+  $("#ws24Low").text(general.wind.speed.low+" knots")
+  // Weather(Sky)
+  let firstPeriodRegion = weaFc24Hr.items[0].periods[0].regions;
+  let secondPeriodRegion = weaFc24Hr.items[0].periods[1].regions;
+  let thirdPeriodRegion = weaFc24Hr.items[0].periods[2].regions;
+
+  //First period
+  let firstPeriod =
+    weaFc24Hr.items[0].periods[0].time.start.slice(11, 19) +
+    " - " +
+    weaFc24Hr.items[0].periods[0].time.end.slice(11, 19);
+  $("#time0").text(firstPeriod);
+  // Weather forecast for first period
+  $("#north0").text(firstPeriodRegion.north);
+  $("#south0").text(firstPeriodRegion.south);
+  $("#east0").text(firstPeriodRegion.east);
+  $("#west0").text(firstPeriodRegion.west);
+  $("#central0").text(firstPeriodRegion.central);
+
+  //Second period
+  let secondPeriod =
+    weaFc24Hr.items[0].periods[1].time.start.slice(11, 19) +
+    " - " +
+    weaFc24Hr.items[0].periods[1].time.end.slice(11, 19);
+  $("#time1").text(secondPeriod);
+  // Weather forecast for second period
+  $("#north1").text(secondPeriodRegion.north);
+  $("#south1").text(secondPeriodRegion.south);
+  $("#east1").text(secondPeriodRegion.east);
+  $("#west1").text(secondPeriodRegion.west);
+  $("#central1").text(secondPeriodRegion.central);
+
+  //Third Period
+
+  let date_tp =
+    date.getFullYear() +
+    "-" +
+    month +
+    "-" +
+    ("0" + (parseInt(day) + 1)).slice(-2);
+  let thirdPeriod =
+    weaFc24Hr.items[0].periods[2].time.start.slice(11, 19) +
+    " - " +
+    weaFc24Hr.items[0].periods[2].time.end.slice(11, 19);
+  $("#time3").text("Time:"+date_tp);
+  $("#time2").text(thirdPeriod);
+  // Weather forecast for first period
+  $("#north2").text(thirdPeriodRegion.north)
+  $("#south2").text(thirdPeriodRegion.south)
+  $("#east2").text(thirdPeriodRegion.east)
+  $("#west2").text(thirdPeriodRegion.west)
+  $("#central2").text(thirdPeriodRegion.central)
+}
+
+function currentWeather(name) {
+  let disp = "<h3>" + name + "</h3>";
+  $("#location div").append(disp);
 }
 // Function to process location and output display
 // Pythagoras function to calculate distance.
@@ -490,4 +550,3 @@ function closestDist(lat, long, lat1, long1) {
   let result = xDiff * xDiff + yDiff * yDiff;
   return result;
 }
-
