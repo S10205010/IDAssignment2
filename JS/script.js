6ed35c15bf9591f693652ab2a29ca9f132b640e2
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-01-04 22:06:44 +0800

    Update Light Dark Mode

--- JS/script.js
+++ JS/script.js
@@ -26,13 +26,13 @@ function lightDark(modeCondition){
     $("body").css({ color: "gray", "background-color": "black" });
     $("#LightDarkMode").css({ "background-color": "black" });
     $(".navbar-brand img").attr("src", "Images/CarpeDiemDark.png");
-    $("#locationSearch").css({"background-color":"#444","border-color":"#999","color":"#aaa"})
+    $("#locationSearch,#startTime select,#endTime select,#button,#eventDate,#scheduleLocation,#eventName").css({"background-color":"#444","border-color":"#999","color":"#aaa"})
   } else {
     $("header nav").removeClass("bg-dark navbar-dark");
     $("header nav").addClass("bg-light navbar-light");
     $("body").css({ color: "black", "background-color": "white" });
     $("#LightDarkMode").css({ "background-color": "white" });
     $(".navbar-brand img").attr("src", "Images/CarpeDiemLight.png");
-    $("#locationSearch").css({"background-color":"#eee","border-color":"black","color":"black"})
+    $("#locationSearch,#startTime select,#endTime select,#button,#eventDate,#scheduleLocation,#eventName").css({"background-color":"#eee","border-color":"black","color":"black"})
   }
 }
