a0d0d09a0e875101cfc547c43b3c2c1e52a28af4
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-01-10 13:20:17 +0800

    Update process task section

--- JS/schedule.js
+++ JS/schedule.js
@@ -33,35 +33,96 @@ function displayTimeOption() {
     $("#smin, #emin, #ssec, #esec").append(option);
   }
 }
+function sortDate(task1,task2){
+  if(task1.eventDate > task2.eventDate){
+    return 1;
+  }
+  else if(task1.eventDate<task2.eventDate){
+    return-1;
+  }
+  else{
+    return 0;
+  }    
+}
+function sortTime(task1,task2){
+  if(task1.startTime > task2.startTime){
+    return 1;
+  }
+  else if(task1.startTime<task2.startTime){
+    return-1;
+  }
+  else{
+    return 0;
+  }
+}
+function TaskObject(eventName, locationIndex,eventDate,startTime,endTime){
+  this.eventName = eventName;
+  this.locationIndex = locationIndex;
+  this.eventDate = eventDate;
+  this.startTime = startTime;
+  this.endTime = endTime;
+}
 function processTask(){
 }
 function displayTasklist(){
+  $("#tasks div").remove();
   $("#tasks").append("<div class='row'></div>");
-
 }
-$(document).ready(function () {
+$(document).ajaxStop(function () {
   displayTimeOption();
   displayEventLocation(area);
   var weaFc4d = JSON.parse(sessionStorage.getItem("4DayFC"));
   let forecast = weaFc4d.items[0].forecasts;
   displayDateOption(forecast);
-
+  
+  if ($(window).width()<=576){
+    $("form div").hide();
+    let formHidden = true;
+  $("form").click(function(){
+    if (formHidden == true){
+      $("form div").show();
+      formHidden = false;
+    }else{
+      $("form div").hide()
+      formHidden = true;
+    }
+  })
+  }
+  let task;
+  if (JSON.parse(localStorage.getItem("task")) == null){
+    task = [];
+  }else{
+    task = JSON.parse(localStorage.getItem("task"));
+  }
   $("#button").click(function (event) {
+    $(".error div").remove();
     event.preventDefault();
     let locationIndex = $("select#scheduleLocation").val();
     let eventName = $("#eventName").val();
     let eventDate = $("select#eventDate").val();
     let startTime =
       $("select#shour").val() +
-      "/" +
+      ":" +
       $("select#smin").val() +
-      "/" +
+      ":" +
       $("select#ssec").val();
     let endTime =
       $("select#ehour").val() +
-      "/" +
+      ":" +
       $("select#emin").val() +
-      "/" +
+      ":" +
       $("select#esec").val();
+    if(eventName.length == 0){
+      $(".error").append("<div>Error, fill in event Name</div>");
+    }
+    else if(endTime<=startTime){
+      $(".error").append("<div>Error, Start time and End Time input error!</div>");
+    }else{
+      let newTask = new TaskObject(eventName, locationIndex,eventDate,startTime,endTime);
+      task.push(newTask);
+      task.sort(sortTime);
+      task.sort(sortDate);
+      localStorage.setItem("task",JSON.stringify(task))
+    }    
   });
 });
